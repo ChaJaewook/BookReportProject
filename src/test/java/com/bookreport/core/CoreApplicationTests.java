b401600facad3d2cacec6aaed13a49ff6be60bd9
ChaJaewook <44600628+ChaJaewook@users.noreply.github.com>
2023-05-11 23:49:11 +0900

    first commit

---
+++ src/test/java/com/bookreport/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookreport.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
