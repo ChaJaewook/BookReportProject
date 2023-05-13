999d1e36b0ee8b359610199271da7d7dd1cfa951
ChaJaewook <44600628+ChaJaewook@users.noreply.github.com>
2023-05-13 17:29:12 +0900

    - TestCase 생성

---
+++ src/test/java/com/bookreport/core/service/BookServiceTest.java
@@ -0,0 +1,40 @@
+package com.bookreport.core.service;
+
+import com.bookreport.core.domain.Book;
+import com.bookreport.core.repository.BookRepository;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class BookServiceTest {
+
+    @Autowired
+    BookService bookService;
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    public void 책추가()
+    {
+        //given
+        Book book=new Book();
+        book.setTitle("바람과 함께 사라지다.");
+
+        //when
+        Long saveId=bookService.save(book);
+
+        //then
+        Assertions.assertEquals(book,bookRepository.findOne(saveId));
+    }
+
+}
---
+++ src/test/java/com/bookreport/core/service/MemberServiceTest.java
@@ -0,0 +1,39 @@
+package com.bookreport.core.service;
+
+import com.bookreport.core.domain.Member;
+import com.bookreport.core.repository.MemberRepository;
+import com.sun.source.tree.MemberReferenceTree;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입()
+    {
+        //given
+        Member member=new Member();
+        member.setName("jaewook");
+
+        //when
+        Long saveId=memberService.join(member);
+
+        //then
+        Assertions.assertEquals(member, memberRepository.findOne(saveId));
+    }
+}
